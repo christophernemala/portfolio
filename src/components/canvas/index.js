8ae38062c47ebcc6b2211f173ad1774ac07b3b29
bharathkdev <bharathaskkarur46@gmail.com>
2026-02-01 18:10:41 +0530

    refactor: Remove unwanted assets and components

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,17 +1,11 @@
-import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
-import StarsCanvas from "./Stars";
 import BusCanvas from "./Bus";
 import PlaneCanvas from "./Plane";
 import LighthouseCanvas from "./Lighthouse";
 import WeatherCanvas from "./Weather";
 
 export { 
-    EarthCanvas, 
     BallCanvas, 
-    ComputersCanvas, 
-    StarsCanvas, 
     BusCanvas, 
     PlaneCanvas, 
     LighthouseCanvas, 
